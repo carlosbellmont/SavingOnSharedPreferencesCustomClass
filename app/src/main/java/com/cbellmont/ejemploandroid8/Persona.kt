9ac97bf3540ccf4c0bb62709692437e50fb02450
Carlos <carlostena@centroafuera.es>
2021-06-11 12:41:58 +0200

    updates

--- app/src/main/java/com/cbellmont/ejemploandroid8/Persona.kt
+++ app/src/main/java/com/cbellmont/ejemploandroid8/Persona.kt
@@ -7,12 +7,12 @@ import kotlinx.serialization.Serializable
 data class Persona(var nombre: String, var edad: Int) {
 
     companion object {
-
         fun fromJson(json : String) : Persona {
             val gson = Gson()
             return gson.fromJson(json, Persona::class.java)
         }
     }
+
 
     fun toJson() : String {
         val gson = Gson()
